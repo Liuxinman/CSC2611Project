210da4b33d9effca17024052947e92a885d0e102
Xinman Liu <xinman2@ualberta.ca>
2022-11-07 14:39:22 -0500

    remove personal data

--- run_make_dataset.sh
+++ run_make_dataset.sh
@@ -1,4 +1,4 @@
 python make_dataset.py \
-    --data_dir=/Users/xinmanliu/Documents/CourseUT/2611/project/CSC2611Project/tweet_dataset \
-    --year=2019 \
-    --months=10,11,12
+    --data_dir=/path/to/tweet_dataset \
+    --year=year_to_retrieve \
+    --months=months_to_retrieve_list
